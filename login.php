abaf0ba5b7d868f546098df45aba4f42bdcbbd5c
anubys <anubys@gmx.com>
2014-03-14 17:22:42 +0000

    kk

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+ <html xmlns="http://www.w3.org/1999/xhtml" lang="es" xml:lang="es">
+<head><title>Inicio sesion</title> 
+ <!--  Añadimos la linea de meta en UTF-8 por el tema de las ñ y demás caracteres especiales esta linea siempre va entre el head -->   
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">  
+<link rel="stylesheet" type="text/css" href="estilo.css">
+</link>
+</meta>
+</head>
+ <body>
+  <FORM ACTION="valida.php" METHOD="post"> 
+<center><b><strong><h2>Formulario de validacion</h2></strong></b><br></center>
+</br><center>
+<b> Usuario</b> : <INPUT TYPE="text" NAME="nombre_usuario" SIZE=20 MAXLENGTH=20>
+<b>Password</b>: <INPUT TYPE="password" NAME="clave_usuario" SIZE=20 MAXLENGTH=20>
+<br></br></center><br>
+<center><INPUT TYPE="submit" CLASS="boton" VALUE="ENTRAR" size="20"></input></center>
+</FORM>
+<br><br>
+</div>
+</body>
+<center>
+</center>
+ </div>
+ </html>
